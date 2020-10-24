442b69e2b1e13985d11d5df471e2727fbc00bf11
Aarshika1 <61321778+Aarshika1@users.noreply.github.com>
2020-10-24 23:53:16 +0200

    Changed server name

--- public_html_as/maintenance/pages/Admin.php
+++ public_html_as/maintenance/pages/Admin.php
@@ -7,10 +7,10 @@ $contact = $_POST['contact'];
 $permission = $_POST['permission'];
 $file = $_POST['file'];
 
-$servername = "localhost";
+$servername = "10.72.1.14
 $username = "aasingh";
 $password = "";
-$db = "MariaDB";
+$db = "group2";
 
 $conn -= new mysqli($servername, $username, $password, $db);
 
