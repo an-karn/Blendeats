ff02fbd279d6e2cc7df00b3618bdafc37a980826
Aarshika1 <61321778+Aarshika1@users.noreply.github.com>
2020-11-08 13:49:21 +0100

    Update member and account

--- insert_commands.sql
+++ insert_commands.sql
@@ -1,38 +1,38 @@
 -- ADDING DATA
 --===============
-INSERT INTO member(uid, Fname, Lname, Country)
+INSERT INTO member(uid, Fname, Lname, Country, email)
 VALUES
-    (1, 'Ankit', 'Karn', 'Nepal'),
-    (2, 'Alex', 'Costa', 'U.S.A'),
-    (3, 'Shubhaman', 'Gill', 'India'),
-    (4, 'Pat', 'Cummins', 'Australia'),
-    (5, 'Sam', 'Curran', 'West Indies');
+    (1, 'Ankit', 'Karn', 'Nepal','a@a.com'),
+    (2, 'Alex', 'Costa', 'U.S.A','a@c.com'),
+    (3, 'Shubhaman', 'Gill', 'India','s@g.com'),
+    (4, 'Pat', 'Cummins', 'Australia','p@c.com'),
+    (5, 'Sam', 'Curran', 'West Indies','s@c.com');
     
-INSERT INTO member(uid, Fname, Lname, Country)
+INSERT INTO member(uid, Fname, Lname, Country,email)
 VALUES
-    (6, 'Ishwor', 'Giri', 'Nepal'),
-    (7, 'Aarshika', 'Singh', 'India'),
-    (8, 'John','Doe', 'Nepal');
+    (6, 'Ishwor', 'Giri', 'Nepal','i@g.com'),
+    (7, 'Aarshika', 'Singh', 'India','a@s.com'),
+    (8, 'John','Doe', 'Nepal','j@d.com');
     
 --Food
-INSERT INTO Food(fid, Title, Price, Image) VALUES  (01, 'Momo', 1.2, 'Momo-pic');
-INSERT INTO Food(fid, Title, Price, Image) VALUES  (02, 'Burger', 2.0, 'Burger-pic');
-INSERT INTO Food(fid, Title, Price, Image) VALUES  (03, 'Biryani', 1.5, 'Biryani-pic');
-INSERT INTO Food(fid, Title, Price, Image) VALUES  (04, 'Pizza', 3.0, 'Pizza-pic');
-INSERT INTO Food(fid, Title, Price, Image) VALUES  (05, 'Noodles', 2.0, 'Noodles-pic');
-INSERT INTO Food(fid, Title, Price, Image) VALUES  (06, 'Pasta', 20.0, 'pasta-pic');
-INSERT INTO Food(fid, Title, Price, Image) VALUES  (07, 'Scones', 29.0, 'scones-pic');
+INSERT INTO food(fid, Title, Price, Image) VALUES  (01, 'Momo', 1.2, 'Momo-pic');
+INSERT INTO food(fid, Title, Price, Image) VALUES  (02, 'Burger', 2.0, 'Burger-pic');
+INSERT INTO food(fid, Title, Price, Image) VALUES  (03, 'Biryani', 1.5, 'Biryani-pic');
+INSERT INTO food(fid, Title, Price, Image) VALUES  (04, 'Pizza', 3.0, 'Pizza-pic');
+INSERT INTO food(fid, Title, Price, Image) VALUES  (05, 'Noodles', 2.0, 'Noodles-pic');
+INSERT INTO food(fid, Title, Price, Image) VALUES  (06, 'Pasta', 20.0, 'pasta-pic');
+INSERT INTO food(fid, Title, Price, Image) VALUES  (07, 'Scones', 29.0, 'scones-pic');
 
 --Admin table
-INSERT INTO Admin(uid, permission_type) VALUES  (6, 'Read,Edit,Delete');
-INSERT INTO Admin(uid, permission_type) VALUES  (7, 'Read,Edit,Delete');
-INSERT INTO Admin(uid, permission_type) VALUES  (8, 'Read');
+INSERT INTO admin(uid, permission_type) VALUES  (6, 'Read,Edit,Delete');
+INSERT INTO admin(uid, permission_type) VALUES  (7, 'Read,Edit,Delete');
+INSERT INTO admin(uid, permission_type) VALUES  (8, 'Read');
 
 --Client table
-INSERT INTO Client(uid, preferred_payment) VALUES  (1, 'Cash');
-INSERT INTO Client(uid, preferred_payment) VALUES  (3, 'Cash');
-INSERT INTO Client(uid, preferred_payment) VALUES  (5, 'Cash');
-INSERT INTO Client(uid, preferred_payment) VALUES  (6, 'PayPal');
+INSERT INTO client(uid, preferred_payment) VALUES  (1, 'Cash');
+INSERT INTO client(uid, preferred_payment) VALUES  (3, 'Cash');
+INSERT INTO client(uid, preferred_payment) VALUES  (5, 'Cash');
+INSERT INTO client(uid, preferred_payment) VALUES  (6, 'PayPal');
 
 --Offers table
 INSERT INTO offers(uid, fid, oid) VALUES (1,1,0987);
@@ -45,35 +45,35 @@ INSERT INTO offers(uid, fid, oid) VALUES (2,6,0998);
 
 
 --Request table
-INSERT INTO Requests(uid, fid, rid)VALUES(1, 4, 0977);
-INSERT INTO Requests(uid, fid, rid)VALUES(2, 1, 0978);
-INSERT INTO Requests(uid, fid, rid)VALUES(3, 5, 0979);
-INSERT INTO Requests(uid, fid, rid)VALUES(4, 1, 0980);
-INSERT INTO Requests(uid, fid, rid)VALUES(5, 3, 0981);
-INSERT INTO Requests(uid, fid, rid)VALUES(5, 1, 0982);
+INSERT INTO requests(uid, fid, rid)VALUES(1, 4, 0977);
+INSERT INTO requests(uid, fid, rid)VALUES(2, 1, 0978);
+INSERT INTO requests(uid, fid, rid)VALUES(3, 5, 0979);
+INSERT INTO requests(uid, fid, rid)VALUES(4, 1, 0980);
+INSERT INTO requests(uid, fid, rid)VALUES(5, 3, 0981);
+INSERT INTO requests(uid, fid, rid)VALUES(5, 1, 0982);
 
 --Packaged food
-INSERT INTO Packaged_Food(fid, bestbefore)VALUES(2, 10);
-INSERT INTO Packaged_Food(fid, bestbefore)VALUES(4, 30);
-INSERT INTO Packaged_Food(fid, bestbefore)VALUES(6, 2);
-INSERT INTO Packaged_Food(fid, bestbefore)VALUES(7, 5);
+INSERT INTO packaged_food(fid, bestbefore)VALUES(2, 10/02/2020);
+INSERT INTO packaged_food(fid, bestbefore)VALUES(4, 30/09/2021);
+INSERT INTO packaged_food(fid, bestbefore)VALUES(6, 02/11/2021);
+INSERT INTO packaged_food(fid, bestbefore)VALUES(7, 15/02/2022);
 
 --Cooked food
-INSERT INTO Cooked_Food(fid, ingredients)VALUES(1, 'Chicken, Vegetables, Wheat');
-INSERT INTO Cooked_Food(fid, ingredients)VALUES(3, 'Chicken, Rice, Indian spices');
-INSERT INTO Cooked_Food(fid, ingredients)VALUES(5, 'Noodles, Spices');
+INSERT INTO cooked_food(fid, ingredients)VALUES(1, 'Chicken, Vegetables, Wheat');
+INSERT INTO cooked_food(fid, ingredients)VALUES(3, 'Chicken, Rice, Indian spices');
+INSERT INTO cooked_food(fid, ingredients)VALUES(5, 'Noodles, Spices');
 
 --Vegan
-INSERT INTO Vegan(fid)VALUES(4);
-INSERT INTO Vegan(fid)VALUES(5);
+INSERT INTO vegan(fid)VALUES(4);
+INSERT INTO vegan(fid)VALUES(5);
 
 --Vegetarian
-INSERT INTO Vegetarian(fid)VALUES(2);
-INSERT INTO Vegetarian(fid)VALUES(7);
+INSERT INTO vegetarian(fid)VALUES(2);
+INSERT INTO vegetarian(fid)VALUES(7);
 
 --non veg halal
 INSERT INTO n_veg_halal(fid)VALUES(3);
 
 --non veg not halal
-INSERT INTO n_veg_n_halal(fid)VALUES(6);
-INSERT INTO n_veg_n_halal(fid)VALUES(1);
+INSERT INTO n_veg(fid)VALUES(6);
+INSERT INTO n_veg(fid)VALUES(1);
